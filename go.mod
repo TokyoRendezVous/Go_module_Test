869ce2ee8cba6bd47615943bf4cbd1aa0a113cfc
TokyoRendezVous <ultra17631767931@gmail.com>
2024-03-15 21:24:04 +0900

    Calc

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github/TokyoRendezVous/Test/v2
+
+go 1.21.3
--- hello.go
+++ hello.go
@@ -5,3 +5,19 @@ import "fmt"
 func Hi() {
 	fmt.Println("Hi!")
 }
+
+func Calc_add(a, b int) int {
+	return a + b
+}
+
+func Calc_sub(a, b int) int {
+	return a - b
+}
+
+func Calc_mul(a, b int) int {
+	return a * b
+}
+
+func Calc_div(a, b int) int {
+	return a / b
+}
